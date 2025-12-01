86aeb110bd9946a21d1306694fc1587c73549d6d
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2025-12-01 23:23:04 +0200

    Update comments in integration and unit test entry points for clarity

--- tests/integration/main.cpp
+++ tests/integration/main.cpp
@@ -1,5 +1,6 @@
 // tests/integration/main.cpp - integration test entry point
-// requires root to run because raw sockets need privileges
+// Defines the main() function for integration tests
+// Requires root privileges
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
